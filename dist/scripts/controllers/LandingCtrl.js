41b8fde54504b599fae76f63a62942ff96257b87
Steve Wichelecki <swichelecki@gmail.com>
2016-12-07 21:11:17 -0600

    Add initial commit 2

--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -1,10 +1,28 @@
 (function() {
-    function LandingCtrl($scope, $firebaseObject) {
-        var ref = firebase.database().ref().child("data");
-        // download the data into a local object
-        var syncObject = $firebaseObject(ref);
-        // synchronize the object with a three-way data binding
-        syncObject.$bindTo($scope, "data");
+    function LandingCtrl($scope, $firebaseArray) {
+        var ref = firebase.database().ref();
+        var task = $firebaseArray(ref); 
+        
+        task.$add({ value: "Pet Nigel" }).then(function(ref) {
+            var id = ref.key;
+            console.log("added record with id " + id);
+            console.log(task.$indexFor(id));
+        });
+        
+        //task.$destroy(33);
+        
+        for (var i = 0; i < task.length; i++) {
+             task.$remove(i);
+    
+        }
+        
+        //task.$remove();
+        //task[3].value = "good";
+        //task.$save(3);
+        //task[3].value = "good";
+        //task.$save(3);
+        
+        $scope.task = task;
     }
  
  
